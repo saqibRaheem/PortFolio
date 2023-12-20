fd4b4c001f187228356244150536077be39b6496
saqibRaheem <saqibraheem1999@gmail.com>
2023-12-20 20:56:39 +0500

    navbar code

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import logos from "./logos.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -31,6 +32,7 @@ import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
+  logos,
   logo,
   backend,
   creator,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,41 @@
 import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
-
+// import {logos} from '../assets/logos.png'
 import { styles } from '../styles';
 import { navLinks } from '../constants'
-import { logo, menu, close} from '../assets'
+import { logos, menu, close} from '../assets'
 
 const Navbar = () => {
+   const[active,setActive] = useState()
+  
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
+        <Link 
+        to='/' 
+        className='flex gap-2 item-center'
+        onClick={()=>{
+          setActive("")
+          window.scrollTo(0,0)
+        }}
+        >
+          <img src={logos} alt="logo" className='object-contain w-9 h-9'/>
+          <p className='text-white text-[18px] font-bold cursor-pointer'> Saqib <span className='hidden sm:block'> Web Developer </span> </p>
+        </Link>
+       <ul className="list-none hidden sm:flex flex-row gap-10">
+        {
+          navLinks.map((link)=>{
+            console.log(link.title);
+            <li>
+              <a className='text-white' href={`#${link.id}`}>{link.title}</a>
+            </li>
+          })
+        }
+       </ul>
+      </div>
+
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar 
