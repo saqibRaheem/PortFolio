665bd57e019f2d4d24e67b25b139d211ea2a9901
saqibRaheem <saqibraheem1999@gmail.com>
2023-12-21 19:34:06 +0500

    navbar code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logos, menu, close} from '../assets'
 
 const Navbar = () => {
    const[active,setActive] = useState()
+   const[toggle,setToggle] = useState(false)
   
   return (
     <nav className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -22,16 +23,33 @@ const Navbar = () => {
           <img src={logos} alt="logo" className='object-contain w-9 h-9'/>
           <p className='text-white text-[18px] font-bold cursor-pointer'> Saqib <span className='hidden sm:block'> Web Developer </span> </p>
         </Link>
+        {/* <p>gfgdgfhgfhg</p> */}
        <ul className="list-none hidden sm:flex flex-row gap-10">
-        {
-          navLinks.map((link)=>{
-            console.log(link.title);
-            <li>
-              <a className='text-white' href={`#${link.id}`}>{link.title}</a>
-            </li>
-          })
-        }
+       {
+        navLinks.map((link)=>{
+            return(
+              <li key={link.id} 
+              className={`${
+                active === link.title
+                ? "text-white"
+                : "text-secondary"
+              } hover:text-white text-[18px]
+              font-medium cursor-pointer`}
+              onClick={()=> setActive(link.title)}>
+                <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+            )
+        })
+       }
        </ul>
+
+       <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img src={menu} alt="menu" 
+        className="w-[28px] h-[28px]
+        object-contain cursor-pointer" 
+        onClick={()=> setToggle(!toggle)}
+        />
+       </div>
       </div>
 
     </nav>
